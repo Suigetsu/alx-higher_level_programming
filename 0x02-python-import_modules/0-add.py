0883cf1299300124e151747c1388df8e19dc1971
Suigetsu <lagmed.019@gmail.com>
2023-09-07 10:24:21 -0700

    added a main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import add_0 as add
-a = 1
-b = 2
-sum = add.add(a, b)
-print(f"{a:d} + {b:d} = {sum:}")
+if __name__ == "__main__":
+	a = 1
+	b = 2
+	sum = add.add(a, b)
+	print(f"{a:d} + {b:d} = {add.add(a, b)}")
