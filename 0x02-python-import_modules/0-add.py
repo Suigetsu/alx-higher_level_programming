9b11a58a77fad60d9843d044ea818752467b2c1a
Suigetsu <lagmed.019@gmail.com>
2023-09-07 10:26:52 -0700

    yarbi

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import add_0 as add
+from add_0 import add
 if __name__ == "__main__":
 	a = 1
 	b = 2
-	sum = add.add(a, b)
-	print(f"{a:d} + {b:d} = {add.add(a, b)}")
+	print(f"{a:d} + {b:d} = {add(a, b)}")
